cb9d115ac3def106c17d950ab1c263c412f99443
sml782 <sml782@163.com>
2020-08-09 17:04:55 +0800

    feat: 分步

--- index.ts
+++ index.ts
@@ -9,8 +9,29 @@ const jimp = require('jimp');
 const jsQR = require('jsqr');
 const pngjs = require('pngjs');
 
-async function mobileLogin(page) {
-  const phoneBtn = await page.$('div.others-item .item-text');
+let workBrowser = null;
+let workPage = null;
+
+async function generateWorkProcess(url = '') {
+  // 找到本机安装的 chrome
+  const findChromePath = await findChrome({});
+  const executablePath = findChromePath.executablePath;
+
+  workBrowser = await puppeteer.launch({
+    headless: false,
+    slowMo: 100,
+    devtools: false,
+    // args: [''],
+    executablePath,
+  });
+
+  workPage = await this.workBrowser.newPage();
+  await workPage.goto(url, { waitUntil: 'networkidle2' });
+  // await page.waitForNavigation();
+}
+
+async function mobileLogin() {
+  const phoneBtn = await workPage.$('div.others-item .item-text');
   phoneBtn.click();
 
   // 用户输入手机号
@@ -26,15 +47,15 @@ async function mobileLogin(page) {
     },
   }];
   const mobileAns = await inquirer.prompt(mobilePromps);
-  await page.type('#pass > div > div > div.login-area > div.mobile-form > div:nth-child(2) > input', mobileAns.mobile);
+  await workPage.type('#pass > div > div > div.login-area > div.mobile-form > div:nth-child(2) > input', mobileAns.mobile);
 
-  await page.waitFor(1000);
+  await workPage.waitFor(1000);
 
   // 发送验证码
-  const verifyBtn = await page.$('#pass > div > div > div.login-area > div.mobile-form > div:nth-child(4) > div > button');
+  const verifyBtn = await workPage.$('#pass > div > div > div.login-area > div.mobile-form > div:nth-child(4) > div > button');
   verifyBtn.click();
 
-  await page.waitFor(1000);
+  await workPage.waitFor(1000);
 
   // 用户输入验证码
   const verifyPromps = [{
@@ -49,17 +70,18 @@ async function mobileLogin(page) {
     },
   }];
   const verifyCodeAns = await inquirer.prompt(verifyPromps);
-  await page.type('#pass > div > div > div.login-area > div.mobile-form > div:nth-child(4) > div > input', verifyCodeAns.verifyCode);
+  await workPage.type('#pass > div > div > div.login-area > div.mobile-form > div:nth-child(4) > div > input', verifyCodeAns.verifyCode);
 
-  await page.waitFor(1000);
+  await workPage.waitFor(1000);
 
   // 点击登录
-  const loginBtn = await page.$('#pass > div > div > div.login-area > div.mobile-form > button');
+  const loginBtn = await workPage.$('#pass > div > div > div.login-area > div.mobile-form > button');
   loginBtn.click();
 }
 
-async function weixinGetImg(browser, iframeUrl) {
-  const page = await browser.newPage();
+// 获取微信登录二维码图片地址, 暂时先不做
+async function weixinGetImg(iframeUrl) {
+  const page = await workBrowser.newPage();
   await page.goto(iframeUrl);
   await page.waitFor('body > div.old-template > div > div > div.waiting.panelContent > div.wrp_code > img');
   const imgsrc = await page.$eval('body > div.old-template > div > div > div.waiting.panelContent > div.wrp_code > img', selector => selector.src);
@@ -69,6 +91,7 @@ async function weixinGetImg(browser, iframeUrl) {
   return fullSrc;
 }
 
+// 解析二维码图片, 后期作为console内容展示给控制台
 // async function decodeImgSrc(imgSrc) {
 //   console.log(imgSrc);
 //   https.get(imgSrc, res => {
@@ -96,28 +119,80 @@ async function weixinGetImg(browser, iframeUrl) {
 //   });
 // }
 
+// 微信登录
 // async function weixinLogin(page) {
-//   const frame = page.frames().find(frame => frame.name() === 'myframe');
+//   const frame = workPage.frames().find(frame => frame.name() === 'myframe');
 // }
 
+async function handleItem(type, itemItem) {
+  if (/点播/.test(type)) {
+    await itemItem.click();
+    await workPage.waitForNavigation({ waitUntil: 'networkidle2' });
+    await workPage.waitForSelector('.');
+    return 
+  }
+}
+
+async function traverseItem(itemList = [], cindex = 0) {
+  if (cindex > itemList.length) {
+    return;
+  }
+
+  const itemItem = itemList[cindex];
+  const itemType = itemItem.$eval('.section-box .contont-title span', ele => ele.textContent);
+  
+}
+
+async function traverseSection(sectionList = [], cindex = 0) {
+  if (cindex > sectionList.length) {
+    return;
+  }
+  const sectionItem = sectionList[cindex];
+  const itemList = await sectionItem.$$('.ivu-timeline .ivu-timeline-item');
+  console.log(`本节有 ${sectionList.length} 条`);
+  
+  await traverseItem(itemList);
+}
+
+async function traverseChapter(chapterList = [], cindex = 0) {
+  if (cindex > chapterList.length) {
+    return;
+  }
+  const chapterItem = chapterList[cindex];
+  const isActive = workPage.evaluate(ele => {
+    ele.classList.contains('ivu-collapse-item-active');
+  }, chapterItem);
+  if (!isActive) {
+    await chapterItem.click();
+  }
+  const sectionList = await chapterItem.$$('.ivu-collapse-content .chapter-item');
+  console.log(`本章有 ${sectionList.length} 节`);
+
+  await traverseSection(sectionList);
+}
+
+async function traverseDake(dakeList = [], cindex = 0) {
+  if (cindex > dakeList.length) {
+    return;
+  }
+  const intoBtn = await dakeList[cindex].$('button.goon-and-review-btn');
+  await intoBtn.click();
+  await workPage.waitForNavigation({ waitUntil: 'networkidle2' });
+  await workPage.waitFor(1000);
+
+  const chapterList = await workPage.$$('div.chapter .ivu-collapse .ivu-collapse');
+  console.log(`本课程有 ${chapterList.length} 章`);
+
+  await traverseChapter(chapterList);
+}
+
 (async () => {
-  const findChromePath = await findChrome({});
-  const executablePath = findChromePath.executablePath;
+  await generateWorkProcess('https://passport.kaikeba.com/login');
 
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 100,
-    devtools: false,
-    // args: [''],
-    executablePath,
-  });
-  const page = await browser.newPage();
-  await page.goto('https://passport.kaikeba.com/login', { waitUntil: 'networkidle2' });
-  // await page.waitForNavigation();
-  await page.waitFor('#login_container > iframe');
-  await page.waitFor(1000);
+  await workPage.waitFor('#login_container > iframe');
+  await workPage.waitFor(1000);
 
-  await page.screenshot({ path: './example.png' });
+  await workPage.screenshot({ path: './example.png' });
 
   const loginPromps = [{
     type: 'list',
@@ -141,7 +216,7 @@ async function weixinGetImg(browser, iframeUrl) {
       console.log(chalk.green('请扫码登录'));
       break;
     case 'mobile':
-      await mobileLogin(page);
+      await mobileLogin();
       break;
   }
 
@@ -152,22 +227,24 @@ async function weixinGetImg(browser, iframeUrl) {
   // console.log(qrcodeSrc);
 
 
-  await page.waitForNavigation();
-  await page.waitFor('#app > div.home-area.router-view > div.type-course > div > div.ivu-tabs-content > div:nth-child(1) > div > div > div > div:nth-child(2) > div.dake');
-  await page.waitFor(2000);
+  await workPage.waitForNavigation();
+  await workPage.waitFor('#app > div.home-area.router-view > div.type-course > div > div.ivu-tabs-content > div:nth-child(1) > div > div > div > div:nth-child(2) > div.dake');
+  await workPage.waitFor(2000);
 
-  const dakeList = await page.$$('#app > div.home-area.router-view > div.type-course > div > div.ivu-tabs-content > div:nth-child(1) > div > div > div > div:nth-child(2) > div.dake');
+  const dakeList = await workPage.$$('#app > div.home-area.router-view > div.type-course > div > div.ivu-tabs-content > div:nth-child(1) > div > div > div > div:nth-child(2) > div.dake');
   console.log(dakeList.length);
 
   const index = 0;
   while (index < dakeList.length) {
     const intoBtn = await dakeList[index].$('button.goon-and-review-btn');
     await intoBtn.click();
-    await page.waitForNavigation({ waitUntil: 'networkidle2' });
-    await page.waitFor(1000);
+    await workPage.waitForNavigation({ waitUntil: 'networkidle2' });
+    await workPage.waitFor(1000);
 
-    const chapterList = await page.$('div.chapter .ivu-collapse .ivu-collapse');
+    const chapterList = await workPage.$$('div.chapter .ivu-collapse .ivu-collapse');
     console.log(chapterList.length);
+
+    break;
   }
 
   // await browser.close();
